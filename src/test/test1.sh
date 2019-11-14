c7c24cf4c261435c31493e547c1c81cab8abd2d7
Jerry Lundström <lundstrom.jerry@gmail.com>
2019-11-14 16:48:11 +0100

    Initial code
    - Initial code of `tinyframe`, a minimalistic Frame Stream library

---
+++ src/test/test1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -xe
+
+./test1 "$srcdir/test.fstrm" 7
+./test1 "$srcdir/test.fstrm" 15
+./test1 "$srcdir/test.fstrm" 24
+./test1 "$srcdir/test.fstrm" 44
+./test1 "$srcdir/test.fstrm" 79
+./test1 "$srcdir/test.fstrm" 134
